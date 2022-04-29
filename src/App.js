500c035b410b92482d3510fa10ff2dfbe88cb1d0
Lawrence <adelegbelawrence@gmail.com>
2022-04-29 17:53:42 +0100

    login setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Userprofile from './Userprofile'
+import { AiFillGithub } from 'react-icons/ai'
+import './style.css'
+
+// Login section
+function Login() {
+  return (
+    <div className="login-card">
+      <h2>Github User Repositories</h2>
+      <p>Login to view your first 20 repositories</p>
+      <button>
+        Login with Github <AiFillGithub />
+      </button>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/user-profile" element={<Userprofile />} />
+
+          {/* <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoutes isLoggedIn={isLoggedIn}>
+              <Dashboard />
+            </ProtectedRoutes>
+          }
+        /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/Userprofile.js
@@ -0,0 +1,13 @@
+const User = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3"></div>
+
+        <div className="col-md-6"></div>
+      </div>
+    </div>
+  )
+}
+
+export default User
